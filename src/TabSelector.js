9760989eacad58e3ce296207c633d6186cfe6073
HugoCravoMendes <hugo.cravomendes@gmail.com>
2021-01-20 17:34:50 +0000

    challenge completed

--- src/TabSelector.js
+++ src/TabSelector.js
@@ -1,41 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class TabSelector extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeId: 'home'
-    };
-    this.handleChangeTab = this.handleChangeTab.bind(this);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     activeId: "home",
+  //   };
+  //   this.handleChangeTab = this.handleChangeTab.bind(this);
+  // }
 
-  handleChangeTab(event) {
-    const buttonId = event.target.id;
-    this.setState({ activeId: buttonId });
-  }
+  // handleChangeTab(event) {
+  //   const buttonId = event.target.id;
+  //   this.setState({ activeId: buttonId });
+  // }
 
   render() {
-    const { activeId } = this.state;
     return (
       <div className="TabSelector">
         <button
           id="home"
-          onClick={this.handleChangeTab}
-          className={activeId === 'home' ? 'active' : ''}
+          onClick={this.props.handleChange}
+          className={this.props.actualId === "home" ? "active" : ""}
         >
           Home
         </button>
         <button
           id="about"
-          onClick={this.handleChangeTab}
-          className={activeId === 'about' ? 'active' : ''}
+          onClick={this.props.handleChange}
+          className={this.props.actualId === "about" ? "active" : ""}
         >
           About
         </button>
         <button
           id="contact"
-          onClick={this.handleChangeTab}
-          className={activeId === 'contact' ? 'active' : ''}
+          onClick={this.props.handleChange}
+          className={this.props.actualId === "contact" ? "active" : ""}
         >
           Contact
         </button>
